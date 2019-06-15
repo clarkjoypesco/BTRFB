163a9feae950c9bf5586bfcf7128c615965bf2fa
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-06-15 18:03:59 +0800

    PUT Request Update Action

--- kontakmanager/src/App.js
+++ kontakmanager/src/App.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Contacts from "./components/contacts/Contacts";
 import AddContact from "./components/contacts/AddContact";
+import EditContact from "./components/contacts/EditContact";
 import Header from "./components/layout/Header";
 import About from "./components/pages/About";
 import NotFound from "./components/pages/NotFound";
@@ -24,6 +25,7 @@ class App extends Component {
                 {/* <Route exact path="/about/:id" component={About} /> */}
                 <Route exact path="/test" component={Test} />
                 <Route exact path="/contact/add" component={AddContact} />
+                <Route exact path="/contact/edit/:id" component={EditContact} />
                 <Route component={NotFound} />
               </Switch>
             </div>
